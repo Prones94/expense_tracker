34fca3b0cc4f01854ac32d1cdc64c563ca1b6ff2
Ian <ronesp.85@gmail.com>
2021-10-14 17:00:47 -0700

    [add] included actions inside of contextReducer file

--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,27 @@
 import React, { useReducer, createContext } from 'react'
 
+import contextReducer from './contextReducer'
+
 const initialState = []
 
-export const expenseTrackerContext = createContext((initialState))
+export const ExpenseTrackerContext = createContext((initialState))
 
 export const Provider = ({ children }) => {
+  const [state, dispatch] = useReducer(contextReducer, initialState)
+
+  //Action Creators
+  const deleteTransaction = (id) => {
+    dispatch({ type: 'DELETE_TRANSACTION', payload: id })
+  }
+
+  const addTransaction = (transaction) => {
+    dispatch({ type: 'ADD_TRANSACTION', payload: transaction })
+  }
   return (
-    <ExpenseTrackerContext.Provider value={{ appName: 'Expense Tracker'}}>
+    <ExpenseTrackerContext.Provider value={{
+      deleteTransaction,
+      addTransaction
+    }}>
       { children }
     </ExpenseTrackerContext.Provider>
   )
